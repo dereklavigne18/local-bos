0ee52ecc601658a9a090d6a287951b69437dded0
Derek Lavigne <derek.lavigne@comcast.net>
2024-07-25 08:11:03 -0400

    Mock business hooks

--- site/src/App.test.tsx
+++ site/src/App.test.tsx
@@ -1,9 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import useBusinessesHooks, { FetchBusinessesState} from 'src/hooks/useBusinesses';
 
 describe('App', () => {
+  const useBusinessesSpy = vi.spyOn(useBusinessesHooks, 'useBusinesses');
+
   it('should render the title', () => {
+    useBusinessesSpy.mockReturnValue(new FetchBusinessesState("fetched", []));
     render(<App />);
     const linkElement = screen.getByText(/Local Bos/i);
     expect(linkElement).toBeInTheDocument();
