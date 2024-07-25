10995e66ec91e64c1a780e8c32046d330898e08d
Derek Lavigne <derek.lavigne@comcast.net>
2024-07-25 08:11:57 -0400

    Add the vi import

--- site/src/App.test.tsx
+++ site/src/App.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import useBusinessesHooks, { FetchBusinessesState} from 'src/hooks/useBusinesses';
